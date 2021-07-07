9b7bf5efdcff100f9d800c141ca61df075b47707
cleocardoso <cleotads21@gmail.com>
2021-07-07 15:37:59 -0300

    Modificando funçoes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,64 +1,109 @@
-import React, { useDebugValue, useState } from 'react';
+import React, { useDebugValue, useState, useEffect } from 'react';
 import { Text, View, StyleSheet, TouchableOpacity, Button } from 'react-native';
-import { MyInput } from '../components/MyInput';
+
 
 
 export function Home() {
 
-    const [result,setResult] = useState('');
-    const [numbers,setNumbers] = useState([]);
+    const [result, setResult] = useState(0);
+    const [numberConcat, setNumberConcat] = useState(''); // esse state vai "acumular" os numeros digitados
+    const [numbers, setNumbers] = useState([]);
+    const [operation, setOperation] = useState('')  
+ 
     
-
-
-    function addNumber (number) {
-        setNumbers(numbers => [...numbers, number])
-    }
-    
-    function resultado(soma) {
-        setResult(soma) // insere o valor da soma
-        setNumbers([]) // reseta os numeros inseridos
-       
-    }
-    console.log(resultado)
-    function soma() {
-        let sum = 0;
-        for (let i = 0; i < numbers.length; i++){
-            sum += parseInt(numbers[i])
+    // 1 + 2 + 5 * 2
+    function addNumber(number) { // 1, 2, 5, 2
+        console.log('Number', number)
+        setNumberConcat(n => n + number)
+        console.log('op', operation !== '')
+        if (operation !== '') {
+            logica(operation) // 3
         }
-        return sum;
     } 
-    function subtracao() {
-        let subtrair = 0;
-        for (let i = 0; i < numbers.length; i++){
-            subtrair -= parseInt(numbers[i])
+
+    function logica(op) { 
+        let i = 0;
+        if (numbers.length > 1) { 
+            const numPrev = numbers[i] // 1
+            const numNext = numbers[i + 1] // 2
+            setNumbers([resultado(op, numPrev, numNext)]) // 3
+            setNumberConcat('')
         }
-        return subtrair;
-    }
-    function dividir() {
-        let div = 0;
-        for (let i = 0; i < numbers.length; i++){
-            div /= parseInt(numbers[i])
+    }  
+
+    function addOperation(op) { // +
+        console.log('Operation', op)
+        setOperation(op) // +
+        if (numberConcat !== ''){
+            numbers.push(parseFloat(numberConcat)) // [8]
         }
-        return div;
+        setNumbers(numbers) 
+        logica(op)
+        setNumberConcat('')
     }
-    function multiplicacao() {
-        let mult = 0;
-        for (let i = 0; i < numbers.length; i++){
-            mult *= parseInt(numbers[i])
+
+    function checkOperation() { // =
+        if (numberConcat !== '') { 
+            numbers.push(parseFloat(numberConcat))
+            setNumbers(numbers) 
+            logica(operation)
+            setNumberConcat('')
+        } 
+    }
+
+    function resultado(operation, numPrev, numNext) {
+        if (numPrev !== undefined && numNext !== undefined) {
+            console.log("[" + numPrev + ", "+numNext +']' ) 
+            switch (operation) {
+                case '+':
+                    return soma(numPrev, numNext);
+                case '-':
+                    return subtracao(numPrev, numNext);
+                case '*':
+                    return multiplicacao(numPrev, numNext)
+                case '/':
+                    return dividir(numPrev, numNext);     
+            }
         }
-        return mult;
+        return 0
     }
-    function limpar(){
-        
+
+    
+
+    function soma(numPrev, numNext) {
+        const sum = numPrev + numNext;
+        setResult(sum)
+        return sum
+    }
+    function subtracao(numPrev, numNext) {
+        const sum = numPrev - numNext;
+        setResult(sum)
+        return sum
+    }
+    function dividir(numPrev, numNext) {
+        const sum = numPrev / numNext;
+        setResult(sum)
+        return sum
+    }
+    function multiplicacao(numPrev, numNext) {
+        const sum = numPrev * numNext;
+        setResult(sum)
+        return sum
+    }
+    function limpar() {
+        setResult(0)
+        setNumbers([]) // reseta os numeros inseridos 
+        setOperation('') 
+
     }
 
     return (
 
         <View style={styles.container}>
             <View style={styles.resultContainer} >
-                <Text style={styles.resultText} >0</Text>
+                <Text style={styles.resultText} >{result}</Text>
             </View>
-            <TouchableOpacity style={styles.ButtonContainer}onPress={() => limpar()}>
+            <TouchableOpacity style={styles.ButtonContainer} onPress={() => limpar()}>
                 <Text style={styles.ButtonText}>CE/C</Text>
             </TouchableOpacity>
             <View style={styles.numberContainer}>
@@ -74,230 +119,230 @@ export function Home() {
                 <TouchableOpacity style={styles.cent2} onPress={() => addNumber(0)}>
                     <Text style={styles.ButtonTextNumber}>0</Text>
                 </TouchableOpacity>
-             <View style={styles.numberContainer2}>
-                <TouchableOpacity style={styles.cent3}onPress={() => addNumber(8)}>
-                    <Text style={styles.ButtonTextNumber}>8</Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.cent3}onPress={() => addNumber(5)}>
-                    <Text style={styles.ButtonTextNumber}>5</Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.cent3}onPress={() => addNumber(2)}>
-                    <Text style={styles.ButtonTextNumber}>2</Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.cent3}onPress={() => addNumber('00')}>
-                    <Text style={styles.ButtonTextNumber}>00</Text>
-                </TouchableOpacity>
-             </View> 
-             <View style={styles.numberContainer3}>
-                <TouchableOpacity style={styles.cent4}onPress={() => addNumber(9)}>
-                    <Text style={styles.ButtonTextNumber}>9</Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.cent4}onPress={() => addNumber(6)}>
-                    <Text style={styles.ButtonTextNumber}>6</Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.cent4}onPress={() => addNumber(3)}>
-                    <Text style={styles.ButtonTextNumber}>3</Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.cent4}onPress={() => addNumber('.')}>
-                    <Text style={styles.ButtonTextNumber}>.</Text>
-                </TouchableOpacity>
-             </View> 
-             <View style={styles.numberContainer4}>
-                <TouchableOpacity style={styles.cent5}>
-                    <Text style={styles.ButtonTextNumber}>%</Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.cent5}>
-                    <Text style={styles.ButtonTextNumber}onPress={() => multiplicacao()}>x</Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.cent7}onPress={() => soma()}>
-                    <Text style={styles.ButtonTextNumberMais}>+</Text>
-                </TouchableOpacity>
-             </View> 
-             <View style={styles.numberContainer5}>
-                <TouchableOpacity style={styles.cent6}>
-                    <Text style={styles.ButtonTextNumber}> {'>'} </Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.cent6}onPress={() => dividir()}>
-                    <Text style={styles.ButtonTextNumber}>÷</Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.cent6}onPress={() => subtracao()}>
-                    <Text style={styles.ButtonTextNumber}>-</Text>
-                </TouchableOpacity>
-                <TouchableOpacity style={styles.cent6}onPress={() => resultado()}>
-                    <Text style={styles.ButtonTextNumber}>=</Text>
-                    
-                </TouchableOpacity>
-             </View> 
+                <View style={styles.numberContainer2}>
+                    <TouchableOpacity style={styles.cent3} onPress={() => addNumber(8)}>
+                        <Text style={styles.ButtonTextNumber}>8</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.cent3} onPress={() => addNumber(5)}>
+                        <Text style={styles.ButtonTextNumber}>5</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.cent3} onPress={() => addNumber(2)}>
+                        <Text style={styles.ButtonTextNumber}>2</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.cent3} onPress={() => addNumber('00')}>
+                        <Text style={styles.ButtonTextNumber}>00</Text>
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.numberContainer3}>
+                    <TouchableOpacity style={styles.cent4} onPress={() => addNumber(9)}>
+                        <Text style={styles.ButtonTextNumber}>9</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.cent4} onPress={() => addNumber(6)}>
+                        <Text style={styles.ButtonTextNumber}>6</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.cent4} onPress={() => addNumber(3)}>
+                        <Text style={styles.ButtonTextNumber}>3</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.cent4} onPress={() => addNumber('.')}>
+                        <Text style={styles.ButtonTextNumber}>.</Text>
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.numberContainer4}>
+                    <TouchableOpacity style={styles.cent5}>
+                        <Text style={styles.ButtonTextNumber}>%</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.cent5}>
+                        <Text style={styles.ButtonTextNumber} onPress={() => addOperation('*')}>x</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.cent7} onPress={() => addOperation('+')}>
+                        <Text style={styles.ButtonTextNumberMais}>+</Text>
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.numberContainer5}>
+                    <TouchableOpacity style={styles.cent6}>
+                        <Text style={styles.ButtonTextNumber}> {'>'} </Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.cent6} onPress={() => addOperation('/')}>
+                        <Text style={styles.ButtonTextNumber}>÷</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.cent6} onPress={() => addOperation('-')}>
+                        <Text style={styles.ButtonTextNumber}>-</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.cent6} onPress={() => checkOperation()}>
+                        <Text style={styles.ButtonTextNumber}>=</Text>
+
+                    </TouchableOpacity>
+                </View>
+            </View>
         </View>
-    </View>
-        
 
 
-         
-        
 
-            );
-    
+
+
+
+    );
+
 }
 
 const styles = StyleSheet.create({
-    container:{
+    container: {
         flex: 1,
-        backgroundColor:'#000000'
-        
-        
-    },
-    resultContainer:{
-        width:340,
-        height:80,
-        marginTop:30,
-        left: 10,
-        backgroundColor:'#FFFFFF',
-        borderRadius:10,
-        
-        
-    },
-    resultText:{
-        top:1,
-        color:'black',
-        fontSize:35,
-        fontWeight:'bold',
-        padding:20,
-        textAlign:'right'
-        
-    },
-    ButtonContainer:{
-        width:90,
-        height:80,
-        marginTop: 30,
-        left: 260,
-        backgroundColor:'#FF8C00',
-        borderRadius:10,
-        justifyContent:'center'
-        
-    },
-    ButtonText:{
-        color:'white',
-        fontSize:20,
-        fontWeight:'bold',
-        padding:20,
-        textAlign:'center'
-        
-    },
-    numberContainer:{
-        flex:1,
-        flexDirection: 'column',
-        justifyContent:'center',
-        
-    },
-    cent2:{
-        width:60,
-        height:60,
-        top:24,
-        left:2,
-        margin:10,
-        marginTop:5,
-        backgroundColor:'#808080',
-        borderRadius:10,
-        
-    },
-    numberContainer2:{
-        flex:1,
-        flexDirection: 'column',
-        justifyContent:'center'
-    },
-    cent3:{
-        width:60,
-        height:60,
-        top:-143,
-        left:70,
-        margin:10,
-        marginTop:5,
-        backgroundColor:'#808080',
-        borderRadius:10,
-        
-    },
-    numberContainer3:{
-        flex:1,
-        flexDirection: 'column',
-        justifyContent:'center'
-    },
-    cent4:{
-        width:60,
-        height:60,
-        top:-175,
-        left:140,
-        margin:10,
-        marginTop:5,
-        backgroundColor:'#808080',
-        borderRadius:10,
-        
-    },
-    numberContainer4:{
-        flex:1,
-        flexDirection: 'column',
-        justifyContent:'center'
-    },
-    cent5:{
-        width:60,
-        height:60,
-        top:-208,
-        left:210,
-        margin:10,
-        marginTop:5,
-        backgroundColor:'#808080',
-        borderRadius:10,
-        
-    },
-    numberContainer5:{
-        flex:1,
-        flexDirection: 'column',
-        justifyContent:'center',
+        backgroundColor: '#000000'
+
 
     },
-    cent6:{
-        width:60,
-        height:60,
-        top:-240,
-        left:280,
-        margin:10,
-        marginTop:5,
-        backgroundColor:'#808080',
-        borderRadius:10,
-        
+    resultContainer: {
+        width: 340,
+        height: 80,
+        marginTop: 30,
+        left: 10,
+        backgroundColor: '#FFFFFF',
+        borderRadius: 10,
+
+
     },
-    cent7:{
-        width:60,
-        height:135,
-        top:-208,
-        left:210,
-        margin:10,
-        marginTop:5,
-        backgroundColor:'#808080',
-        borderRadius:10,
-        
+    resultText: {
+        top: 1,
+        color: 'black',
+        fontSize: 35,
+        fontWeight: 'bold',
+        padding: 20,
+        textAlign: 'right'
+
     },
-    ButtonTextNumber:{
-        color:'white',
-        fontSize:18,
-        fontWeight:'bold',
-        padding:20,
-        textAlign:'center'
-        
+    ButtonContainer: {
+        width: 90,
+        height: 80,
+        marginTop: 30,
+        left: 260,
+        backgroundColor: '#FF8C00',
+        borderRadius: 10,
+        justifyContent: 'center'
+
     },
-    ButtonTextNumberMais:{
-        top:30,
-        color:'white',
-        fontSize:18,
-        fontWeight:'bold',
-        padding:20,
-        textAlign:'center'
-        
+    ButtonText: {
+        color: 'white',
+        fontSize: 20,
+        fontWeight: 'bold',
+        padding: 20,
+        textAlign: 'center'
+
     },
-    
-       
-      
-   
-   
+    numberContainer: {
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'center',
+
+    },
+    cent2: {
+        width: 60,
+        height: 60,
+        top: 24,
+        left: 2,
+        margin: 10,
+        marginTop: 5,
+        backgroundColor: '#808080',
+        borderRadius: 10,
+
+    },
+    numberContainer2: {
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'center'
+    },
+    cent3: {
+        width: 60,
+        height: 60,
+        top: -143,
+        left: 70,
+        margin: 10,
+        marginTop: 5,
+        backgroundColor: '#808080',
+        borderRadius: 10,
+
+    },
+    numberContainer3: {
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'center'
+    },
+    cent4: {
+        width: 60,
+        height: 60,
+        top: -175,
+        left: 140,
+        margin: 10,
+        marginTop: 5,
+        backgroundColor: '#808080',
+        borderRadius: 10,
+
+    },
+    numberContainer4: {
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'center'
+    },
+    cent5: {
+        width: 60,
+        height: 60,
+        top: -208,
+        left: 210,
+        margin: 10,
+        marginTop: 5,
+        backgroundColor: '#808080',
+        borderRadius: 10,
+
+    },
+    numberContainer5: {
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'center',
+
+    },
+    cent6: {
+        width: 60,
+        height: 60,
+        top: -240,
+        left: 280,
+        margin: 10,
+        marginTop: 5,
+        backgroundColor: '#808080',
+        borderRadius: 10,
+
+    },
+    cent7: {
+        width: 60,
+        height: 135,
+        top: -208,
+        left: 210,
+        margin: 10,
+        marginTop: 5,
+        backgroundColor: '#808080',
+        borderRadius: 10,
+
+    },
+    ButtonTextNumber: {
+        color: 'white',
+        fontSize: 18,
+        fontWeight: 'bold',
+        padding: 20,
+        textAlign: 'center'
+
+    },
+    ButtonTextNumberMais: {
+        top: 30,
+        color: 'white',
+        fontSize: 18,
+        fontWeight: 'bold',
+        padding: 20,
+        textAlign: 'center'
+
+    },
+
+
+
+
+
 
 });
